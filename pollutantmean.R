28c16e4885203366991e824cf329fa24d77c5d6c
Aditya Dubey <65142287+adityastats@users.noreply.github.com>
2020-05-17 17:09:14 +0530

    Delete pollutantmean.R

--- pollutantmean.R
+++
@@ -1,9 +0,0 @@
-pollutantmean <- function(directory,pollutant,id=1:332){
-  files <- list.files(directory,full.names = TRUE)
-  data <- data.frame()
-  for (i in id){
-    x <- read.csv(files[i])
-    data <- rbind(data,x)
-  }
-  mean(data[["sulfate"]],na.rm = TRUE)
-}
